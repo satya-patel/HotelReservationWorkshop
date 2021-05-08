6e910069b673c5571117211093b82d381f757dea
satya prakash patel <82859781+satya-patel@users.noreply.github.com>
2021-05-08 14:22:03 +0530

    Add files via upload

---
+++ hotel workshop/src/com/reservation/CustomerType.java
@@ -0,0 +1,12 @@
+package com.reservation;
+
+public enum CustomerType {
+    Rewards, Regular;
+
+    public static CustomerType from(String rawRequest) {
+        if (rawRequest.contains(Rewards.name())) {
+            return Rewards;
+        }
+        return Regular;
+    }
+}
