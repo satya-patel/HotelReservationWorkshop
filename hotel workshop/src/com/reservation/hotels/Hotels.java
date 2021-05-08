20f6e4e54365eb3ae1ca460d415e85daadc53895
satya prakash patel <82859781+satya-patel@users.noreply.github.com>
2021-05-08 14:34:54 +0530

    uc2 cheapest rate with base rate

---
+++ hotel workshop/src/com/reservation/hotels/Hotels.java
@@ -0,0 +1,21 @@
+package com.reservation.hotels;
+
+import com.reservation.ReservationRequest;
+
+public class Hotels {
+    private Hotel[] hotels;
+
+    public Hotels(Hotel... hotels) {
+        this.hotels = hotels;
+    }
+
+    public String hotelWithBestRateFor(ReservationRequest request) {
+        Hotel hotelWithBestRate = hotels[0];
+        for (Hotel hotel : hotels) {
+            if (hotel.priceFor(request) < hotelWithBestRate.priceFor(request)) {
+                hotelWithBestRate = hotel;
+            }
+        }
+        return hotelWithBestRate.getHotelName();
+    }
+}
