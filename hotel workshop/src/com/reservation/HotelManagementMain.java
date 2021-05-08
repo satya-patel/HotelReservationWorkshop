a616aee0f43b9f731f1430e94efc99310f3fe43f
satya prakash patel <82859781+satya-patel@users.noreply.github.com>
2021-05-08 13:21:02 +0530

    welcome file added

---
+++ hotel workshop/src/com/reservation/HotelManagementMain.java
@@ -0,0 +1,14 @@
+package com.reservation;
+
+import com.reservation.hotels.DefaultHotels;
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String... args) throws IOException {
+        System.out.println("Welcome to hotel management!");
+    }
+}
