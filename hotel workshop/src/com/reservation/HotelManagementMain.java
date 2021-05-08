9211ea06d9763aec55a0830a80dc72def73a48cb
satya prakash patel <82859781+satya-patel@users.noreply.github.com>
2021-05-08 16:37:24 +0530

    Main method code added

--- hotel workshop/src/com/reservation/HotelManagementMain.java
+++ hotel workshop/src/com/reservation/HotelManagementMain.java
@@ -7,8 +7,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class Main {
+public class HotelManagementMain {
     public static void main(String... args) throws IOException {
         System.out.println("Welcome to hotel management!");
+        
+        String fileLocation = "input/sampleInput.txt";
+        List<String> lines = Files.readLines(new File(fileLocation), Charsets.UTF_8);
+
+        ReservationSystem reservationSystem = new ReservationSystem(lines, new DefaultHotels().hotels());
+        System.out.println(reservationSystem.reservationResults());
     }
 }
